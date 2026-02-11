e2f990a74ec27768e00debe839378acb869d8891
gaddam-suvarna <228A1A0504@risekrishnasaiprakasam.edu.in>
2026-02-12 00:14:16 +0530

    Array Reversed

--- MyArraysDSAlgosProject/src/main/java/AlternateElements.java
+++ MyArraysDSAlgosProject/src/main/java/AlternateElements.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class AlternateElements {
 
-    public  static void main(String[] args) {
+    /*public  static void main(String[] args) {
 
         System.out.println("Enter the size");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int array[] = new int[n];
+        int[] array = new int[n];
 
         System.out.println("Enter the elements");
         for(int i = 0; i < n ; i ++) {
@@ -17,28 +17,28 @@ public class AlternateElements {
         for(int i = 0; i < n ; i += 2) {
             System.out.println(array[i]);
 
+        }*/
+    //printing the alternate numbers using the recursion method
+    static void printAlternate(int array[] , int index) {
+        if(index >= array.length) {
+            return;
         }
+        System.out.println(array[index] +" ");
+        printAlternate(array, index+2);//method calling
 
-
-        }
 
     }
-
-
-
-
-
-
-
-     /*static int arr[] = {10,20,30,40,50};
-    // Iterate over all alternate elements
     public static void main(String[] args) {
-        System.out.println("Alternate numbers from array");
+        System.out.println("enter the array size");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //Enter teh array size
+        int array[] = new int[n];//array creation
+        System.out.println("Enter the elements");
+        for(int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Alternate numbers in array");
+        printAlternate(array, 0);
+    }
 
-        System.out.println(arr[0]);
-        System.out.println(arr[2]);
-        System.out.println(arr[4]);
-
-
-        }*/
-
+}
